e01b4b095d3efdee0dfa15762e86fc746d86c532
mambo-tango <369534543@qq.com>
2018-01-08 16:30:30 +0800

    first commit

---
+++ batfish-webmagic/src/main/java/org/batfish/webmagic/ProducerApp.java
@@ -0,0 +1,18 @@
+package org.batfish.webmagic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author Tango
+ *
+ */
+
+@SpringBootApplication
+public class ProducerApp {
+    
+    public static void main( String[] args ) {
+        SpringApplication.run(ProducerApp.class, args);
+    }
+}
