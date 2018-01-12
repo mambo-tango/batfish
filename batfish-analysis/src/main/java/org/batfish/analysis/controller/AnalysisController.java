3c03a887a5558518c940c943aabc152e0f5d74a8
mambo-tango <369534543@qq.com>
2018-01-12 23:16:55 +0800

    springcloud config配置

--- batfish-analysis/src/main/java/org/batfish/analysis/controller/AnalysisController.java
+++ batfish-analysis/src/main/java/org/batfish/analysis/controller/AnalysisController.java
@@ -2,6 +2,8 @@ package org.batfish.analysis.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,5 +25,24 @@ public class AnalysisController {
     public String analysisTechnology(@RequestParam(value="url") String url, @RequestParam(value="detail") String detail) {
         LOG.error("url===> "+ url + " detail==> " + detail);
         return "receive the msg !";
+    }
+    
+//    @Value("${batfish.analysis.redis.host}")
+//    String redisHost;
+//    
+//    @RequestMapping(value = "/redis")
+//    public String redis(){
+//        return redisHost;
+//    }
+    @Value("${batfish.analysis.redis.host}")
+    String foo;
+    
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        System.out.println("=========================================> "+foo);
+        System.out.println("=========================================> "+foo);
+        System.out.println("=========================================> "+foo);
+        System.out.println("=========================================> "+foo);
+        return foo;
     }
 }
